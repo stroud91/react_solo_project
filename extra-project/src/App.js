619d195ba6e74dd1d889744ba5ffec551919bedf
Ledian Fekaj <stroud91official@gmail.com>
2023-07-08 00:36:40 -0400

    exercise 2

--- extra-project/src/App.js
+++ extra-project/src/App.js
@@ -2,16 +2,28 @@ import { Header } from './components/Header';
 import { Footer } from './components/Footer';
 import './App.css';
 import React from 'react';
-
+import { useState } from 'react';
 export default function App() {
-
-
+  const [count, setCount] = useState(0)
+   function handleAdd() {
+    setCount(count + 1)
+   }
+   function handleSub(){
+    setCount(count - 1)
+   }
+   function handleReset(){
+    setCount(0)
+   }
   return (
     <>
     <Header />
-    <div>
-    <h1>My name is</h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, perspiciatis?</p>
+    <div className='App'>
+    <div className='box'>
+      <p>{count}</p>
+      <button onClick={handleAdd} className='add'>ADD</button>
+      <button onClick={handleSub} className='sub'>SUB</button>
+      <button onClick={handleReset} className='reset'>RESET</button>
+    </div>
     </div>
     <Footer />
     </>
